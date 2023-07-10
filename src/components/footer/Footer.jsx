e5efa0d1466fa5d89f46a7841ccc891ecf1f4839
wafic khalife <wafic.khalife@lau.edu>
2023-07-10 20:19:18 +0300

    29/06/2023 changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,17 +3,18 @@ import "./Footer.css"
 function Footer() {
     return (
         <>
-        <footer className='mainfoot'>
-        <h2 className='Logo'>
+            <footer className='mainfoot'>
+                <h2 className='Logo'>
                     ZINA's CARS
-        </h2>
-        <a href="/privacy">Privacy Policy</a>
-        <p>
-            © 2023 ZINA's CARS. All Rights reserved.
-        </p>
+                </h2>
+                <span className='logojj'>Luxury for you</span>
+                <a href="/privacy">Privacy Policy</a>
+                <p>
+                    © 2023 ZINA's CARS. All Rights reserved.
+                </p>
 
 
-        </footer>
+            </footer>
 
 
         </>
